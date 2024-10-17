d7e2843584916bf9e2471b58427f7e0241cc950a
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-17 19:17:45 +0100

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- attributes: id, integer, never null, 
 -- auto increment and primary key
 -- email, string (255 characters), never null and unique
--- name, string (255 characters
+-- name, string (255 characters)
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
