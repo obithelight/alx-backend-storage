c026f0d9c2e21149d0ca11a075faacaaeb5f3b86
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-26 03:53:17 +0100

    redis cleanup

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,29 +1,78 @@
 #!/usr/bin/env python3
-''' A Python3 Module '''
-
+""" Redis client module
+"""
 import redis
 from uuid import uuid4
 from functools import wraps
 from typing import Any, Callable, Optional, Union
 
 
-class Cache:
-    ''' A Cache Class '''
+def count_calls(method: Callable) -> Callable:
+    """ Decorator for Cache class methods to track call count
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """ Wraps called method and adds its call count redis before execution
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """ Decorator for Cache class method to track args
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args) -> str:
+        """ Wraps called method and tracks its passed argument by storing
+            them to redis
+        """
+        self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
+        output = method(self, *args)
+        self._redis.rpush(f'{method.__qualname__}:outputs', output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable) -> None:
+    """ Check redis for how many times a function was called and display:
+            - How many times it was called
+            - Function args and output for each call
+    """
+    client = redis.Redis()
+    calls = client.get(fn.__qualname__).decode('utf-8')
+    inputs = [input.decode('utf-8') for input in
+              client.lrange(f'{fn.__qualname__}:inputs', 0, -1)]
+    outputs = [output.decode('utf-8') for output in
+               client.lrange(f'{fn.__qualname__}:outputs', 0, -1)]
+    print(f'{fn.__qualname__} was called {calls} times:')
+    for input, output in zip(inputs, outputs):
+        print(f'{fn.__qualname__}(*{input}) -> {output}')
+
+
+class Cache:
+    """ Caching class
+    """
     def __init__(self) -> None:
-        ''' Cache object initialization TASK 0'''
+        """ Initialize new cache object
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        ''' Stores data in cache class '''
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes,  int,  float]) -> str:
+        """ Stores data in redis with randomly generated key
+        """
         key = str(uuid4())
         client = self._redis
         client.set(key, data)
         return key
 
     def get(self, key: str, fn: Optional[Callable] = None) -> Any:
-        ''' Method that converts value from redis TASK 1'''
+        """ Gets key's value from redis and converts
+            result byte  into correct data type
+        """
         client = self._redis
         value = client.get(key)
         if not value:
@@ -37,18 +86,11 @@ class Cache:
         return value
 
     def get_str(self, data: bytes) -> str:
-        ''' conversion function to str from bytes '''
-        return data.decode("UTF-8")
+        """ Converts bytes to string
+        """
+        return data.decode('utf-8')
 
     def get_int(self, data: bytes) -> int:
-        ''' conversion function to int from bytes '''
+        """ Converts bytes to integers
+        """
         return int(data)
-
-    def count_calls(self, method: Callable) -> Callable:
-        ''' counts the Cache class method TASK 2'''
-        @wraps(method)
-        def wrapper(self: Any, *args, **kwargs) -> str:
-            ''' this defines the wrapper method '''
-            self.redis.incr(method.__qualname__)
-            return method(self, *args, **kwargs)
-        return wrapper
