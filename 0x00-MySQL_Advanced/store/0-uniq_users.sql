a74c5f9ff20b8ee5837f86270c69aaa06bf9d60a
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-31 08:25:31 +0100

    final commit

--- 0x00-MySQL_Advanced/store/0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- Check if the users table exists
--- id, email, name
-
-CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-)
